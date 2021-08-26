ec1c678f394960507efb4d68927e255d03476447
nakul parulekar <nakulparulekar@gmail.com>
2021-08-26 12:49:26 +0530

    4th module

---
+++ WishList/Data/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+        
+    }
+}
